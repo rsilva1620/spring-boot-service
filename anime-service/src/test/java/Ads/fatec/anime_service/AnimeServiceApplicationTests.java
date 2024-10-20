d552fb922b6f46d6c175763c1d5e5ad9a6e4c0ee
Ricardo Alexandre <silva1620costa@gmail.com>
2024-10-20 16:14:16 -0400

    inicio do comit do projeto

---
+++ anime-service/src/test/java/Ads/fatec/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Ads.fatec.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
